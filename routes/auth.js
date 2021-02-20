5352e480d92a4554086009b2d36eb552c68b5891
Normorad <normoradnadir@gmail.com>
2021-02-21 10:36:35 +1100

    draft login api route complete

--- routes/auth.js
+++ routes/auth.js
@@ -14,7 +14,44 @@ module.exports = (app) => {
     });
 
     app.post("/api/login", async (req, res) => {
-        
+        const { errors, isValid } = validateLoginInput(req.body);
+
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const user = await User.findOne({ email: email });
+
+        if (!user) {
+            return res.status(404).json({
+                error: "Could not find a user with that email"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (isMatch) {
+            const payload = {
+                id : user.id,
+                name : user.name,
+            };
+
+            let accessToken = jwt.sign(payload, keys.secretOrKey, {
+                expiresIn: 7889231 * 4
+            });
+
+            res.json({
+                success: true,
+                token: "Bearer " + accessToken,
+            });
+        } else {
+            return res.status(400).json({
+                error: "That username and password combination could not be found."
+            })
+        }
     });
 
     app.post("/api/logout", async (req, res) => {
